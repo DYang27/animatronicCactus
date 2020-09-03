8b7a9bbe1c5298eeb214162f1ff0fa256ea291f1
DYang27 <dorothyang27@gmail.com>
2020-09-03 10:51:44 -0400

    Add servos' functionality

--- animatronic_cactus_code/animatronic_cactus_code.ino
+++ animatronic_cactus_code/animatronic_cactus_code.ino
@@ -1,22 +1,26 @@
 #include <Servo.h>
 
-Servo servo1;
-Servo servo2;
-const int servo1Pin = 9;
-const int servo2Pin = 10;
+Servo mouthServo;
+Servo armsServo;
+const int mouthPin = 9;
+const int armsPin = 10;
 const int buttonPin = 2;
 bool servosState = false;
 
-
-int pos = 0;
+int mouthServoPos = 90;
+int armsServoPos = 90;
+int mouthServoDir = 1;
+int armsServoDir = 1;
 int buttonState = 0;
 
 void setup() {
   Serial.begin(9600);
-  servo1.attach(servo1Pin);
-  servo2.attach(servo2Pin);
+  mouthServo.attach(mouthPin);
+  armsServo.attach(armsPin);
   pinMode(buttonPin, INPUT);
 
+  mouthServo.write(mouthServoPos);
+  armsServo.write(armsServoPos);
   buttonState = digitalRead(buttonPin);
   while(!buttonState) {
     Serial.println("initial setup");
@@ -34,9 +38,37 @@ void loop() {
   }
   if(servosState) {
     //turn on servos
+    if(mouthServoDir) {
+      mouthServoPos++;
+      if(mouthServoPos >= 110) {
+        mouthServoDir = !mouthServoDir;
+      }
+    } else {
+      mouthServoPos--;
+      if(mouthServoPos <= 45) {
+        mouthServoDir = !mouthServoDir;
+      }
+    }
+    if(armsServoDir) {
+      armsServoPos--;
+      if(armsServoPos <= 55) {
+        armsServoDir = !armsServoDir;
+      }
+    } else {
+      armsServoPos++;
+      if(armsServoPos >= 125) {
+        armsServoDir = !armsServoDir;
+      }
+    }
+    mouthServo.write(mouthServoPos);
+    armsServo.write(armsServoPos);
+    delay(15);
     Serial.println("servos on");
   } else {
     //turn off servos
+    mouthServo.write(mouthServoPos);
+    armsServo.write(armsServoPos);
+    delay(15);
     Serial.println("servos off");
   }
 }
